55460740aa8c9e068435d4fc5e1142ec0e13f686
djibzo <djibzo90@gmail.com>
2025-05-27 15:13:06 +0000

    auth/me ok

--- user-service/src/main/java/com/eshop/userservice/controller/AuthController.java
+++ user-service/src/main/java/com/eshop/userservice/controller/AuthController.java
@@ -2,18 +2,23 @@ package com.eshop.userservice.controller;
 import com.eshop.userservice.dto.AuthRequest;
 import com.eshop.userservice.dto.AuthResponse;
 import com.eshop.userservice.dto.RegisterRequest;
+import com.eshop.userservice.entity.User;
+import com.eshop.userservice.repository.UserRepository;
 import com.eshop.userservice.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
+    private final UserRepository userRepository;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService,UserRepository userRepository) {
         this.authService = authService;
+        this.userRepository=userRepository;
     }
 
     @PostMapping("/register")
@@ -27,6 +32,16 @@ public class AuthController {
         String token = authService.authenticate(request);
         return ResponseEntity.ok(new AuthResponse(token));
     }
+    @GetMapping("/me")
+    public ResponseEntity<RegisterRequest> me(Authentication authentication) {
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Utilisateur non trouvé"));
+
+        RegisterRequest userDTO = new RegisterRequest(user.getEmail(), user.getUsername(), user.getRole());
+        return ResponseEntity.ok(userDTO);
+    }
+
 
 }
 
--- user-service/src/main/java/com/eshop/userservice/dto/RegisterRequest.java
+++ user-service/src/main/java/com/eshop/userservice/dto/RegisterRequest.java
@@ -1,11 +1,19 @@
 package com.eshop.userservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class RegisterRequest {
     private String email;
     private String password;
     private String role;
     private String username;
+
+    public RegisterRequest(String email, String role, String username) {
+        this.email = email;
+        this.role = role;
+        this.username = username;
+    }
 }
